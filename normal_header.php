a5af25a026c76288f05b4c26e0e434731fa6724a
Azazel5 <subhanga2013@gmail.com>
2020-04-14 13:18:21 -0500

    Cleanup

---
+++ normal_header.php
@@ -0,0 +1,23 @@
+<head>
+<link rel="stylesheet" href="css/normal_header.css">
+</head>
+
+    <nav class="navbar navbar-expand-md tm-navbar" id="tmNav">    
+                <img src="augmend_logo.png" width="90" height="80" style="left:0;">
+        <div class="container">              
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars navbar-toggler-icon"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                  <a id="link1" class="nav-link tm-nav-link" href="#introduction"></a>
+              </li>
+              <li class="nav-item">
+                  <a id="link2" class="nav-link tm-nav-link" href="#contact"></a>
+              </li>     
+
+            </ul>
+          </div>        
+        </div>
+    </nav>
